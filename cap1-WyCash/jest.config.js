27d1bdde420225745f1cc143d90af7ba7615e288
Tiago <tiagohs.dev@gmail.com>
2022-06-06 01:02:10 -0300

    configuração do jest

---
+++ cap1-WyCash/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ["<roootDir>/src"],
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+};
